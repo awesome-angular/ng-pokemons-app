51d88b5de2e6b21dc8e37f592f027c1ca6c2922c
Simon Dieny <simon.dieny@gmail.com>
2018-06-06 15:11:43 +0200

    2.components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
 	selector: 'pokemon-app',
-	template: `<h1>Hello, {{name}} !</h1>`,
+	template: '<h1>Pokémons</h1>'
 })
-export class AppComponent { name = 'Angular'; }
+export class AppComponent implements OnInit {
+
+	pokemons: Pokemon[] = null;
+
+	ngOnInit() {
+		this.pokemons = POKEMONS;
+	}
+
+	selectPokemon(pokemon: Pokemon) {
+		console.log('Vous avez selectionné ' + pokemon.name);
+	}
+
+}
