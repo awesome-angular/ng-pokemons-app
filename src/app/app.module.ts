6770f6852d521c855df8a5b12d0d25e0b7a43ba7
Simon Dieny <simon.dieny@gmail.com>
2018-06-06 17:33:15 +0200

    5.pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @NgModule({
 	imports: [BrowserModule],
-	declarations: [AppComponent, BorderCardDirective],
+	declarations: [
+		AppComponent,
+		BorderCardDirective,
+		PokemonTypeColorPipe
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
