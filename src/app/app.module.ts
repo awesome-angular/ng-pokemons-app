08cca3b754e8f22a41c20fe9368d95133dd4f49f
Simon Dieny <simon.dieny@gmail.com>
2018-06-06 16:53:07 +0200

    4.directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
 
 @NgModule({
 	imports: [BrowserModule],
-	declarations: [AppComponent],
+	declarations: [AppComponent, BorderCardDirective],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
