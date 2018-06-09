64b91262a99b0832348e93da563ef3a02c17af60
Simon Dieny <simon.dieny@gmail.com>
2018-06-09 18:21:10 +0200

    12.authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginRoutingModule } from './login-routing.module';
 import { PokemonsModule } from './pokemons/pokemons.module'
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -14,15 +17,18 @@ import { InMemoryDataService } from './in-memory-data.service';
 	imports: [
 		BrowserModule,
 		HttpClientModule,
+		FormsModule,
 		// Le module HttpClientInMemoryWebApiModule intercepte les requêtes HTTP
 		// et retourne des réponses simulées du serveur.
 		// Retirer l'importation quand un "vrai" serveur est prêt pour recevoir des requêtes.
 		HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
 		PokemonsModule,
+		LoginRoutingModule,
 		AppRoutingModule
 	],
 	declarations: [
 		AppComponent,
+		LoginComponent,
 		PageNotFoundComponent
 	],
 	bootstrap: [AppComponent]
