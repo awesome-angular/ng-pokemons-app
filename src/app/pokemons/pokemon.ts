091dce79e54939d7f81091bdee939a2dc8548b67
Simon Dieny <simon.dieny@gmail.com>
2018-06-09 16:47:55 +0200

    feat(http): add POST request

--- src/app/pokemons/pokemon.ts
+++ src/app/pokemons/pokemon.ts
@@ -6,4 +6,20 @@ export class Pokemon {
 	picture: string;
 	types: Array<string>;
 	created: Date;
+
+	constructor(
+		hp: number = 100,
+		cp: number = 10,
+		name: string = 'name',
+		picture: string = 'http://...',
+		types: Array<string> = ['Normal'],
+		created: Date = new Date()
+	) {
+		this.hp = hp;
+		this.cp = cp;
+		this.name = name;
+		this.picture = picture;
+		this.types = types;
+		this.created = created;
+	}
 }
--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -6,6 +6,7 @@ import { PokemonRoutingModule } from './pokemons-routing.module';
 import { ListPokemonComponent } from './list-pokemon.component';
 import { DetailPokemonComponent } from './detail-pokemon.component';
 import { EditPokemonComponent } from './edit-pokemon.component';
+import { AddPokemonComponent } from './add-pokemon.component';
 import { PokemonFormComponent } from './pokemon-form.component';
 import { BorderCardDirective } from './border-card.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
@@ -21,6 +22,7 @@ import { PokemonsService } from './pokemons.service';
 		ListPokemonComponent,
 		DetailPokemonComponent,
 		EditPokemonComponent,
+		AddPokemonComponent,
 		PokemonFormComponent,
 		BorderCardDirective,
 		PokemonTypeColorPipe
