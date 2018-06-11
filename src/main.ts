34756719637ebae1c8068e514f7eed577faf168e
Simon Dieny <simon.dieny@gmail.com>
2018-06-11 12:02:59 +0200

    feat(deploy): add Systemjs config for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+
+if (!/localhost/.test(document.location.host)) {
+	enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
 	.catch(err => console.log(err));
